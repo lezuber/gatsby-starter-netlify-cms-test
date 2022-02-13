5958ed1954c82b8002c9f542b00b76efbbe5060f
Lenz Zuber <mail@lezuber.com>
2022-02-13 13:19:43 +0000

    wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         },
       },
     },
+    "gatsby-plugin-postcss",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: "gatsby-source-filesystem",
